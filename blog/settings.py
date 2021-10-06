c6fc27bbf4989a13aaeed0c29ac5858cc471971b
Bruno Urbano <bruno.urbano.rocha@gmail.com>
2021-10-05 22:48:15 -0300

    fix media root + filter pluraliza

--- blog/settings.py
+++ blog/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = [BASE_DIR / 'media']
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
